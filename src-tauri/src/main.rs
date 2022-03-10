36a60e9be2f6349a74883cf8ba18057a65990999
Andrew Hudson <ahudson@gmail.com>
2022-03-10 10:16:16 +0000

    feat(close/open apps): close and open apps dependent on ui timer

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,32 +3,12 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{Menu, MenuItem, Submenu};
 use std::process::Command;
-
-
-
-
-// use std::process::Command;
-
-// fn toggle_dark_mode() -> std::process::Output {
-
-//   println!("I was invoked from JS!");
-
-//   Command::new("osascript")
-//       .arg("-e")
-//       .arg("tell application \"System Events\"")
-//       .arg("tell appearance preferences")
-//       .arg("set dark mode to not dark mode")
-//       .arg("end tell")
-//       .arg("end tell")
-//       .stdout(std::process::Stdio::inherit())
-//       .output()
-//       .expect("failed to execute process");
-// }
 
 #[tauri::command]
 fn toggle_dark_mode() {
-  println!("toggle_dark_mode was invoked from JS!");
+  // println!("toggle_dark_mode was invoked from JS!");
 
   Command::new("osascript")
         .arg("-e")
@@ -39,7 +19,7 @@ fn toggle_dark_mode() {
 
 #[tauri::command]
 fn turn_off_apps() {
-  println!("turn_off_apps was invoked from JS!");
+  // println!("turn_off_apps was invoked from JS!");
   Command::new("osascript")
         .arg("-e")
         .arg("tell application \"Safari\" to if it is running then quit")
@@ -48,17 +28,59 @@ fn turn_off_apps() {
         .arg("-e")
         .arg("tell application \"Google Chrome Canary\" to if it is running then quit")
         .output()
+        .expect("failed to quit Apps");
+}
+
+#[tauri::command]
+fn turn_on_apps() {
+  // println!("turn_on_apps was invoked from JS!");
+  Command::new("osascript")
+        .arg("-e")
+        .arg("tell application \"Safari\" to activate")
+        .arg("-e")
+        .arg("tell application \"Tidal\" to activate")
+        .arg("-e")
+        .arg("tell application \"Google Chrome Canary\" to activate")
+        .output()
         .expect("failed to start Safari");
 }
 
-// Also in main.rs
-fn main() {
-  tauri::Builder::default()
-    // This is where you pass in your commands
-    .invoke_handler(tauri::generate_handler![toggle_dark_mode, turn_off_apps])
-    .run(tauri::generate_context!())
-    .expect("failed to run app");
+#[tauri::command]
+fn toggle_tidal() {
+  // println!("turn_on_apps was invoked from JS!");
+  Command::new("osascript")
+        .arg("-e")
+        .arg("tell application \"Safari\" to activate")
+        .arg("-e")
+        .arg("tell application \"Tidal\" to activate")
+        .arg("-e")
+        .arg("tell application \"Google Chrome Canary\" to activate")
+        .output()
+        .expect("failed to start Safari");
 }
 
+
+
+
+// // Also in main.rs
+fn main() {
+  let menu = Menu::new()
+      .add_submenu(Submenu::new("appname", 
+        Menu::new()
+        .add_native_item(MenuItem::Hide)
+        .add_native_item(MenuItem::Separator)
+        .add_native_item(MenuItem::Quit)
+        ));
+
+  // Minimize
+  tauri::Builder::default()
+    // This is where you pass in your commands
+    .invoke_handler(tauri::generate_handler![toggle_dark_mode, turn_off_apps, turn_on_apps, toggle_tidal])
+    .menu(menu)
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
+
+
+}
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,173 @@
 import React, { useCallback, useEffect, useState } from "react";
+
 import "./App.scss";
 import { invoke } from "@tauri-apps/api/tauri";
 
 const THIRTY_MINS = 60 * 30 * 1000;
+// const THIRTY_MINS = 60 * 1000;
+const TIME_REDUCTION = 1000;
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [playing, setPlaying] = useState(false);
-  const [paused, setPaused] = useState(false);
+const Time = ({
+  playing,
+  reset,
+  handleReset,
+  onTimeEnd,
+}: {
+  playing: boolean;
+  reset: boolean;
+  handleReset: () => void;
+  onTimeEnd: () => void;
+}) => {
+  const [count, setCount] = useState(THIRTY_MINS);
 
-  const incrementTimer = useCallback(() => setCount((c) => c + 1), []);
-  const decrementTimer = useCallback(() => setCount((c) => c - 1), []);
+  const decrementTimer = useCallback(
+    () => setCount((c) => c - TIME_REDUCTION),
+    []
+  );
 
   useEffect(() => {
-    const timer = setInterval(incrementTimer, 1000);
+    let timer: any;
 
-    console.log("here");
+    if (playing) {
+      timer = setInterval(decrementTimer, TIME_REDUCTION);
+    } else {
+      clearInterval(timer);
+    }
 
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, [playing, decrementTimer]);
 
-  const handleStart = useCallback(() => {
-    setPlaying(true);
+  useEffect(() => {
+    if (count === 0) {
+      onTimeEnd();
+    }
+  }, [count, onTimeEnd]);
+
+  useEffect(() => {
+    if (reset) {
+      setCount(THIRTY_MINS);
+      handleReset();
+    }
+  }, [reset, setCount, handleReset]);
+
+  return (
+    <div className="timer">
+      <span className="digits">
+        {("0" + Math.floor((count / 60000) % 60)).slice(-2)}:
+      </span>
+      <span className="digits">
+        {("0" + Math.floor((count / 1000) % 60)).slice(-2)}
+      </span>
+    </div>
+  );
+};
+
+const Controls = ({
+  playing,
+  handleStart,
+  handleStop,
+  handleReset,
+  handlePause,
+}: {
+  playing: boolean;
+  handleStart: () => void;
+  handleStop: () => void;
+  handleReset: () => void;
+  handlePause: () => void;
+}) => {
+  return (
+    <div className="buttons">
+      <button onClick={handleStart} disabled={playing}>
+        Play
+      </button>
+      <button onClick={handleReset} disabled={!playing}>
+        Reset
+      </button>
+      <button onClick={handleStop} disabled={!playing}>
+        Stop
+      </button>
+      <button onClick={handlePause}>Pause</button>
+    </div>
+  );
+};
+
+function App() {
+  const [playing, setPlaying] = useState(false);
+  const [reset, setReset] = useState(false);
+
+  useEffect(() => {
+    // add this JS snippet somewhere in your app
+    // window.addEventListener("contextmenu", (e) => e.preventDefault());
   }, []);
 
   const handlePause = useCallback(() => {
     setPlaying((p) => !p);
-    setPaused((p) => !p);
   }, []);
 
   const handleStop = useCallback(() => {
     setPlaying(false);
   }, []);
 
-  const turnOffApps = useCallback(() => {
-    invoke("turn_off_apps");
+  const handleReset = useCallback(() => {
+    setPlaying(false);
+    setReset(true);
   }, []);
 
-  const fireEvent = useCallback(() => {
-    invoke("toggle_dark_mode");
+  const turnOffApps = useCallback(() => {
+    if ("__TAURI_IPC__" in window) {
+      invoke("turn_off_apps");
+    }
   }, []);
+
+  const turnOnApps = useCallback(() => {
+    if ("__TAURI_IPC__" in window) {
+      invoke("turn_on_apps");
+    }
+  }, []);
+
+  const toggleDarkMode = useCallback(() => {
+    if ("__TAURI_IPC__" in window) {
+      invoke("toggle_dark_mode");
+    }
+  }, []);
+
+  const handleStart = useCallback(() => {
+    setPlaying(true);
+    turnOffApps();
+  }, [turnOffApps]);
+
+  const onTimeEnd = useCallback(() => {
+    setPlaying(false);
+    setReset(true);
+    turnOnApps();
+    toggleDarkMode();
+  }, [toggleDarkMode, turnOnApps]);
 
   return (
     <div className="App">
-      <h1>{count}</h1>
+      <h1>
+        <Time
+          playing={playing}
+          reset={reset}
+          handleReset={() => setReset(false)}
+          onTimeEnd={onTimeEnd}
+        />
+      </h1>
 
-      <p>{playing ? "stop" : "play"}</p>
-
-      <div className="buttons">
-        <button onClick={handleStart} disabled={playing}>
-          Play
-        </button>
-        <button onClick={handlePause}>Pause</button>
-        <button onClick={handleStop} disabled={!playing}>
-          Stop
-        </button>
-      </div>
+      <Controls
+        playing={playing}
+        handleStart={handleStart}
+        handleStop={handleStop}
+        handleReset={handleReset}
+        handlePause={handlePause}
+      />
 
       <button onClick={turnOffApps}>Turn off Apps</button>
+      <button onClick={turnOnApps}>Turn On Apps</button>
 
-      <button onClick={fireEvent}>Toggle Dark Mode</button>
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
     </div>
   );
 }
